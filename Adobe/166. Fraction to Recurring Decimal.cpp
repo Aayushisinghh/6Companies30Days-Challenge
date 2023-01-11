92f88793ae9f253a8e6055912643aa69bb29f0fe
Aayushisinghh <89346845+Aayushisinghh@users.noreply.github.com>
2023-01-11 23:52:37 +0530

    Update 166. Fraction to Recurring Decimal.cpp

--- Adobe/166. Fraction to Recurring Decimal.cpp
+++ Adobe/166. Fraction to Recurring Decimal.cpp
@@ -1,30 +1,44 @@
 class Solution {
 public:
-    int maxPoints(vector<vector<int>>& points) {
-        int res = 0;
-        for(int i = 0; i<points.size(); i++){
-            
-            unordered_map<long double,int> noOfPointsInLine;
-            
-            for(int j = i + 1; j < points.size(); j++){
-                // Two points  -  (x1,y1) and (x2,y2) for calculation slope 
-                int x1 = points[i][0], y1 = points[i][1], x2 = points[j][0], y2 = points[j][1];
-                
-                if(y2 == y1){         // y2 == y1 means slope is 0
-                    noOfPointsInLine[INT_MIN]++;
-                }else if(x1 == x2){   // x2 == x1 means slope is infinite
-                    noOfPointsInLine[INT_MAX]++;
-                }else{                // other cases solpe b/w 0 to infinite
-                    long double slope = (long double)(y2 - y1) /(long double)(x2 - x1);
-                    noOfPointsInLine[slope]++;
-                }
-                
+    string fractionToDecimal(int numerator, int denominator) {
+        int isNegative = false;
+        if((numerator < 0 && denominator > 0)||(numerator > 0 && denominator < 0)){
+            isNegative = true;
+        }
+        
+        //now take numerator and denominator +ve for calculation
+        long long n = abs(numerator);
+        long long d = abs(denominator);
+    
+        long long q = n / d;
+        long long r = n % d;
+        
+        string ans = to_string(q);
+        if(r==0) {
+            if(isNegative){
+            ans = "-" + ans;
             }
-            // calculate no. of points in one line
-            for(auto i : noOfPointsInLine){
-                res = max(i.second,res);
+            return ans;
+        }
+        else ans.push_back('.');
+        
+        unordered_map<int,int> mp; //{remender,position}
+
+        while(r != 0){
+            r = r * 10;
+            ans.push_back(r / d + '0');
+            r = r % d;
+            if(mp.count(r) > 0){
+                ans = ans.substr(0,mp[r]) + "(" + ans.substr(mp[r]) + ")";
+                break;
+            }else{
+                mp[r] = ans.size();
             }
         }
-        return res + 1;
+        
+        if(isNegative){
+            ans = "-" + ans;
+        }
+        return ans;
     }
 };
